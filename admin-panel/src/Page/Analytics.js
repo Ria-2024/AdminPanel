e028922da611e82630440e98a15cc8fa602e1c1e
Ria-2024 <90swalapyar@gmail.com>
2024-07-17 00:32:44 +0000

    code refactored

--- admin-panel/src/Page/Analytics.js
+++ admin-panel/src/Page/Analytics.js
@@ -12,7 +12,7 @@ const AnalyticsPage = () => {
   useEffect(() => {
     const fetchAnalyticsData = async () => {
       try {
-        const response = await await axios.get(`${BACKEND_BASE}/getAnalytics`);
+        const response =  await axios.get(`${BACKEND_BASE}/getAnalytics`);
         setAnalyticsData(response.data);
       } catch (error) {
         console.error('Error fetching analytics data:', error);
