fbb3c4dc1f117c5ebc721d54e94323574bf0e231
RiaMeets <90swalapyar@gmail.com>
2024-09-21 11:07:10 -0400

    style changes made

--- admin-panel/src/Page/Landing.js
+++ admin-panel/src/Page/Landing.js
@@ -1,25 +1,48 @@
 // src/pages/LandingPage.js
-import React from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <Container className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
-      <Row>
-        <Col className="d-flex justify-content-around mx-2">
-          <Button variant="primary" onClick={() => navigate('/analytics')} className="mx-2" style={{backgroundColor:"rgb(72, 39, 110)"}}>
+    <Container
+      className="d-flex flex-column align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Row className="w-100">
+        <Col className="d-flex flex-column flex-md-row justify-content-around mx-2">
+          <Button
+            variant="primary"
+            onClick={() => navigate("/analytics")}
+            className="mx-2 my-2 my-md-0"
+            style={{ backgroundColor: "rgb(72, 39, 110)" }}
+          >
             Analytics
           </Button>
-          <Button variant="primary" onClick={() => navigate('/users')} className="mx-2" style={{backgroundColor:"rgb(72, 39, 110)"}}>
+          <Button
+            variant="primary"
+            onClick={() => navigate("/users")}
+            className="mx-2 my-2 my-md-0"
+            style={{ backgroundColor: "rgb(72, 39, 110)" }}
+          >
             Users
           </Button>
-          <Button variant="primary" onClick={() => navigate('/premium')} className="mx-2" style={{backgroundColor:"rgb(72, 39, 110)"}}>
+          <Button
+            variant="primary"
+            onClick={() => navigate("/premium")}
+            className="mx-2 my-2 my-md-0"
+            style={{ backgroundColor: "rgb(72, 39, 110)" }}
+          >
             Applications
           </Button>
-          <Button variant="primary" onClick={() => navigate('/premiumMembers')} className="mx-2" style={{backgroundColor:"rgb(72, 39, 110)"}}>
+          <Button
+            variant="primary"
+            onClick={() => navigate("/premiumMembers")}
+            className="mx-2 my-2 my-md-0"
+            style={{ backgroundColor: "rgb(72, 39, 110)" }}
+          >
             Premium Members
           </Button>
         </Col>
