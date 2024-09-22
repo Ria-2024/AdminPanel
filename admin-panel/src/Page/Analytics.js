d0822f58ec7f6a9ce8a528569b1e09f73e7875fd
RiaMeets <90swalapyar@gmail.com>
2024-09-22 15:26:16 -0400

    sort button added

--- admin-panel/src/Page/Analytics.js
+++ admin-panel/src/Page/Analytics.js
@@ -1,25 +1,28 @@
 // src/components/AnalyticsPage.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Table, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
-import { BACKEND_BASE } from './constant';
-import { useNavigate } from 'react-router-dom';
+import { BACKEND_BASE } from "./constant";
+import { useNavigate } from "react-router-dom";
 
 const AnalyticsPage = () => {
   const [analyticsData, setAnalyticsData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [sortConfig, setSortConfig] = useState({ key: 'timestamp', direction: 'asc' });
+  const [sortConfig, setSortConfig] = useState({
+    key: "timestamp",
+    direction: "asc",
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAnalyticsData = async () => {
       try {
         const response = await axios.get(`${BACKEND_BASE}/getAnalytics`);
-        setAnalyticsData(response.data);
+        setAnalyticsData(response.data.reverse());
       } catch (error) {
-        console.error('Error fetching analytics data:', error);
+        console.error("Error fetching analytics data:", error);
       } finally {
         setLoading(false);
       }
@@ -28,33 +31,33 @@ const AnalyticsPage = () => {
     fetchAnalyticsData();
   }, []);
 
-  const sortedData = React.useMemo(() => {
+  const sortedData = () => {
     let sortableData = [...analyticsData];
     if (sortConfig !== null) {
       sortableData.sort((a, b) => {
         if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === 'asc' ? -1 : 1;
+          return sortConfig.direction === "asc" ? -1 : 1;
         }
         if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === 'asc' ? 1 : -1;
+          return sortConfig.direction === "asc" ? 1 : -1;
         }
         return 0;
       });
     }
     return sortableData;
-  }, [analyticsData, sortConfig]);
+  };
 
   const requestSort = (key) => {
-    let direction = 'asc';
-    if (sortConfig.key === key && sortConfig.direction === 'asc') {
-      direction = 'desc';
+    let direction = "asc";
+    if (sortConfig.key === key && sortConfig.direction === "asc") {
+      direction = "desc";
     }
     setSortConfig({ key, direction });
   };
 
   const getClassNamesFor = (name) => {
     if (sortConfig.key === name) {
-      return sortConfig.direction === 'asc' ? 'asc' : 'desc';
+      return sortConfig.direction === "asc" ? "asc" : "desc";
     }
     return;
   };
@@ -62,22 +65,60 @@ const AnalyticsPage = () => {
   return (
     <Container>
       <h1 className="my-4">Analytics Data</h1>
-      <Button className="sticky-back-button my-2" onClick={() => navigate('/')} style={{backgroundColor:"rgb(72, 39, 110)"}}>Back</Button>
+      <Button
+        className="sticky-back-button my-2"
+        onClick={() => navigate("/")}
+        style={{ backgroundColor: "rgb(72, 39, 110)" }}
+      >
+        Back
+      </Button>
+      <Button
+        className="sticky-back-button my-2 mx-2"
+        onClick={() => setAnalyticsData(sortedData())}
+        style={{ backgroundColor: "rgb(72, 39, 110)" }}
+      >
+        Sort
+      </Button>
       {loading ? (
         <p>Loading...</p>
       ) : (
         <Table responsive="sm" striped bordered hover>
           <thead>
             <tr>
-              <th onClick={() => requestSort('profileID')} className={getClassNamesFor('profileID')}>PID</th>
-              <th onClick={() => requestSort('screenId')} className={getClassNamesFor('screenId')}>Screen</th>
-              <th onClick={() => requestSort('component')} className={getClassNamesFor('component')}>Component</th>
-              <th onClick={() => requestSort('action')} className={getClassNamesFor('action')}>Action</th>
-              <th onClick={() => requestSort('timestamp')} className={getClassNamesFor('timestamp')}>Timestamp</th>
+              <th
+                onClick={() => requestSort("profileID")}
+                className={getClassNamesFor("profileID")}
+              >
+                PID
+              </th>
+              <th
+                onClick={() => requestSort("screenId")}
+                className={getClassNamesFor("screenId")}
+              >
+                Screen
+              </th>
+              <th
+                onClick={() => requestSort("component")}
+                className={getClassNamesFor("component")}
+              >
+                Component
+              </th>
+              <th
+                onClick={() => requestSort("action")}
+                className={getClassNamesFor("action")}
+              >
+                Action
+              </th>
+              <th
+                onClick={() => requestSort("timestamp")}
+                className={getClassNamesFor("timestamp")}
+              >
+                Timestamp
+              </th>
             </tr>
           </thead>
           <tbody>
-            {sortedData.map((event, index) => (
+            {analyticsData.map((event, index) => (
               <tr key={index}>
                 <td>{event.profileID}</td>
                 <td>{event.screenId}</td>
